f06473afcda260cde2adc64e6380806ff99f8c07
davidveecisco <davidvee@cisco.com>
2022-05-16 22:59:48 +0100

    Second

---
+++ POC_SalesCollab/Scripts/site.js
@@ -0,0 +1,30 @@
+function iOS() {
+    return [
+        'iPad Simulator',
+        'iPhone Simulator',
+        'iPod Simulator',
+        'iPad',
+        'iPhone',
+        'iPod'
+    ].includes(navigator.platform)
+        // iPad on iOS 13 detection
+        || (navigator.userAgent.includes("Mac") && "ontouchend" in document)
+}
+
+function dialNumber() {
+
+    var selectedNumber = $('#numberSelect').find(":selected").val();
+    var linkRef = "";
+
+    if (selectedNumber == "0") {
+        alert('Please select a Number');}
+    else {
+        if (iOS() == true) {
+            linkRef = 'sms://+' + selectedNumber + '&body=Get%20started%20by%20sending%20this%20message%20in%20to%20sign%20up';
+        }
+        else {
+            linkRef = 'sms://+' + selectedNumber + '?&body=Get%20started%20by%20sending%20this%20message%20in%20to%20sign%20up';
+        }
+    }
+    window.location = linkRef;
+}
