13b8c2376af9ea7f9932411ba26f11557eacc129
davidveecisco <davidvee@cisco.com>
2022-08-04 09:59:01 +0100

    showcase rcs update

--- POC_SalesCollab/Scripts/site.js
+++ POC_SalesCollab/Scripts/site.js
@@ -11,19 +11,22 @@ function iOS() {
         || (navigator.userAgent.includes("Mac") && "ontouchend" in document)
 }
 
-function dialNumber() {
+function dialNumber(bodyText) {
 
     var selectedNumber = $('#numberSelect').find(":selected").val();
     var linkRef = "";
 
+    if (bodyText === undefined) {
+        bodyText = 'Get%20started%20by%20sending%20this%20message%20in%20to%20sign%20up';
+    }
     if (selectedNumber == "0") {
         $('#errorModal').modal('show');}
     else {
         if (iOS() == true) {
-            linkRef = 'sms://+' + selectedNumber + '&body=Get%20started%20by%20sending%20this%20message%20in%20to%20sign%20up';
+            linkRef = 'sms://+' + selectedNumber + '&body=' + bodyText;
         }
         else {
-            linkRef = 'sms://+' + selectedNumber + '?&body=Get%20started%20by%20sending%20this%20message%20in%20to%20sign%20up';
+            linkRef = 'sms://+' + selectedNumber + '?&body=' + bodyText;
         }
     window.location = linkRef;
     }
