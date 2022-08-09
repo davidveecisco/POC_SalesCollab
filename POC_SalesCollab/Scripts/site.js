d593f5b708ac233899b958bd693e3b079f3f2d52
davidveecisco <davidvee@cisco.com>
2022-08-09 15:06:14 +0100

    showcase general

--- POC_SalesCollab/Scripts/site.js
+++ POC_SalesCollab/Scripts/site.js
@@ -11,6 +11,24 @@ function iOS() {
         || (navigator.userAgent.includes("Mac") && "ontouchend" in document)
 }
 
+var getUrlParameter = function getUrlParameter(sParam) {
+    var sPageURL = window.location.search.substring(1),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+        }
+    }
+    return false;
+};
+
+
+
 function dialNumber(bodyText) {
 
     var selectedNumber = $('#numberSelect').find(":selected").val();
